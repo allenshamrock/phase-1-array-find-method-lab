73408e5a0db39ea2beab464a5d81023aea2fbca5
Allen <allenshamrock37@gmail.com>
2023-12-20 17:11:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+const superbowlWin = function(record){
+  const winningRecord = record.find((record) => record.result === 'W');
+//console.log(winningRecord)  
+ return winningRecord ? winningRecord.year : undefined;
+}
+
+superbowlWin();
